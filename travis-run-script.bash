4c48c4ef4282b857d8405255e5ccc4b567532466
Phil Jackson <phil@shellarchive.co.uk>
2012-09-16 23:43:18 +0100

    Fix travis logic.

--- travis-run-script.bash
+++ travis-run-script.bash
@@ -7,15 +7,14 @@ export MY_TWERP_OPTIONS="--exit-on-failure --runner=simple"
 
 # cant do proxy yet because of the tests that rely on host files being
 # set.
-for project in api base; do
+for project in base api; do
   pushd ${project}
 
   echo "Installing modules..."
   npm install &>/dev/null
 
-  if [[ ${project} -ne "base" ]]; then
-    echo "Linking base..."
-    npm link ../base &>/dev/null
+  if [[ ${project} != "base" ]]; then
+    npm link ../base
   fi
 
   export MY_TWERP_OPTIONS="--exit-on-failure --runner=simple"
