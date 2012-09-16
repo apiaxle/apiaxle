ef97657ae1674dd63100f56abf53a2a9450f7d50
Phil Jackson <phil@shellarchive.co.uk>
2012-09-16 23:16:37 +0100

    Tidy the travis run script a bit.

--- travis-run-script.bash
+++ travis-run-script.bash
@@ -7,18 +7,19 @@ export MY_TWERP_OPTIONS="--exit-on-failure --runner=simple"
 
 # cant do proxy yet because of the tests that rely on host files being
 # set.
-for project in api; do
+for project in api base; do
   pushd ${project}
 
-  npm install
-  npm link ../base
+  echo "Installing modules..."
+  npm install &>/dev/null
+
+  if ${project} -ne "base"; then
+    echo "Linking base..."
+    npm link ../base &>/dev/null
+  fi
+
+  export MY_TWERP_OPTIONS="--exit-on-failure --runner=simple"
   cake test
 
   popd &> /dev/null
 done
-
-# now base
-cd base
-npm install
-npm link ../base
-cake test
