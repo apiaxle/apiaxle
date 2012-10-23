baf142ef975b530626d7055c1d4a1665f48a7e95
Phil Jackson <phil@shellarchive.co.uk>
2012-10-23 11:50:47 +0100

    Make the travis script less silent if it fails.

--- travis-run-script.bash
+++ travis-run-script.bash
@@ -5,16 +5,26 @@ set -e
 export NODE_ENV=test
 export MY_TWERP_OPTIONS="--exit-on-failure --runner=simple"
 
+# only output anything if a command fails
+function silence-or-loud-on-error {
+  output=$(${@} 2>&1)
+
+  if [[ ${?} != 0 ]]; then
+    echo "${output}"
+  fi
+}
+
 # cant do proxy yet because of the tests that rely on host files being
 # set.
 for project in base api; do
   pushd ${project}
 
   echo "Installing modules..."
-  npm install &>/dev/null
+  silence-or-loud-on-error npm install
 
   if [[ ${project} != "base" ]]; then
-    npm link ../base
+    echo "Linking base..."
+    silence-or-loud-on-error npm link ../base
   fi
 
   export MY_TWERP_OPTIONS="--exit-on-failure --runner=simple"
