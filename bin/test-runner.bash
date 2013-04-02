fb9c7d18f6de37106114b3351a193d5b5040b18f
Phil Jackson <phil@shellarchive.co.uk>
2013-04-02 19:30:44 +0100

    Avoid running tests when log files have changed.

--- bin/test-runner.bash
+++ bin/test-runner.bash
@@ -3,13 +3,23 @@
 set -e
 
 while /bin/true; do
-  change=$(inotifywait -qre close_write --format "%w" .)
+  change=$(inotifywait -qre close_write --format "%w %f" .)
   dir=$(echo ${change} | perl -nle 'm#\./([^/]+)# and print $1')
 
-  if [[ -f "${dir}/Cakefile" ]]; then
-    pushd "${dir}"
-    cake test
-    popd
+  # only if there's a cakefile in the directory
+  if [[ ! -f "${dir}/Cakefile" ]]; then
+    continue
   fi
+
+  # not on log files
+  if echo "${change}" | egrep '\.log$' &>/dev/null; then
+    continue
+  fi
+
+  echo "valid enough change: ${change}"
+
+  pushd "${dir}"
+  cake test || echo "Failed."
+  popd
 done
   
