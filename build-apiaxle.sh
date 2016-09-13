fa681c44c356c70bc7d184da0355565af9c56c42
Evan Griffiths <griffithse@gmail.com>
2016-09-13 17:14:22 -0400

    more circleci tweaks

--- build-apiaxle.sh
+++ build-apiaxle.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
-mkdir -p /app/node_modules && cd /app && \
-  ln -s /app/apiaxle/base /app/node_modules/apiaxle-base && \
-  ln -s /app/apiaxle/api /app/node_modules/apiaxle-api && \
-  cp /app/apiaxle/base/package.json /app/ && npm install && \
-  cp /app/apiaxle/api/package.json /app/ && npm install && \
-  cp /app/apiaxle/proxy/package.json /app/ && npm install && \
-  cp /app/apiaxle/repl/package.json /app/ && npm install && \
-  cd /app/apiaxle && make clean && make && \
-  cd /app/apiaxle/repl && \
-  echo "#!/usr/bin/env node" > apiaxle && cat ./apiaxle.js >> apiaxle && \
-  chmod a+x apiaxle && ln -s /app/apiaxle/repl/apiaxle /usr/bin/apiaxle
+
+cd /app/apiaxle && make clean && make && \
+
+cd /app/apiaxle/base && npm install && \
+ln -s /app/apiaxle/base node_modules/apiaxle-base && \
+
+mv node_modules /app/apiaxle/api/ && \
+cd /app/apiaxle/api && \
+npm install && \
+ln -s /app/apiaxle/api node_modules/apiaxle-api && \
+
+mv node_modules /app/apiaxle/proxy/ && \
+cd /app/apiaxle/proxy && \
+npm install && \
+
+mv node_modules /app/apiaxle/repl/ && \
+cd /app/apiaxle/repl && \
+npm install && \
+echo "#!/usr/bin/env node" > apiaxle && cat ./apiaxle.js >> apiaxle && \
+chmod a+x apiaxle && ln -s /app/apiaxle/repl/apiaxle /usr/bin/apiaxle && \
+
+mv node_modules /app/
