e7d087a688bd4bb5614d2065c101f849e7908f2a
Phillip Shipley <phillip_shipley@sil.org>
2016-09-20 15:59:57 -0400

    travis debugging 4

--- build-apiaxle.sh
+++ build-apiaxle.sh
@@ -20,6 +20,6 @@ mv node_modules $BASEDIR/repl/ && \
 cd $BASEDIR/repl && \
 npm install && \
 echo "#!/usr/bin/env node" > apiaxle && cat ./apiaxle.js >> apiaxle && \
-chmod a+x apiaxle && ln -s $BASEDIR/repl/apiaxle /usr/local/bin/apiaxle && \
+chmod a+x apiaxle && mv apiaxle $BASEDIR/ && \
 
 mv node_modules $BASEDIR/
