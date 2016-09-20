47016bccc6510f76ed42f55fad86e3acba887700
Phillip Shipley <phillip_shipley@sil.org>
2016-09-20 15:24:18 -0400

    build updates to hopefully fix travis

--- Dockerfile
+++ Dockerfile
@@ -8,15 +8,15 @@ COPY ./proxy/ /app/apiaxle/proxy/
 COPY ./repl/ /app/apiaxle/repl/
 COPY ./coffeelint.json /app/apiaxle/
 COPY ./Makefile /app/apiaxle/
-COPY ./build-apiaxle.sh /build-apiaxle.sh
+COPY ./build-apiaxle.sh /app/build-apiaxle.sh
 COPY ./docker-entrypoint.sh /app/apiaxle/
 COPY ./apiaxle-config.json /apiaxle-config.json
 
-WORKDIR /app
+WORKDIR /app/apiaxle
 
 RUN apk add --update alpine-sdk python python-dev && \
     npm install -g coffee-script && \
-    /build-apiaxle.sh && \
+    /app/build-apiaxle.sh && \
     apk del --purge alpine-sdk python python-dev && \
     npm uninstall -g coffee-script
 
--- build-apiaxle.sh
+++ build-apiaxle.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
-cd /app/apiaxle && make clean && make && \
+BASEDIR=`pwd`
 
-cd /app/apiaxle/base && npm install && \
-ln -s /app/apiaxle/base node_modules/apiaxle-base && \
+make clean && make && \
 
-mv node_modules /app/apiaxle/api/ && \
-cd /app/apiaxle/api && \
+cd $BASEDIR/base && npm install && \
+ln -s $BASEDIR/base node_modules/apiaxle-base && \
+
+mv node_modules $BASEDIR/api/ && \
+cd $BASEDIR/api && \
 npm install && \
-ln -s /app/apiaxle/api node_modules/apiaxle-api && \
+ln -s $BASEDIR/api node_modules/apiaxle-api && \
 
-mv node_modules /app/apiaxle/proxy/ && \
-cd /app/apiaxle/proxy && \
+mv node_modules $BASEDIR/proxy/ && \
+cd $BASEDIR/proxy && \
 npm install && \
 
-mv node_modules /app/apiaxle/repl/ && \
-cd /app/apiaxle/repl && \
+mv node_modules $BASEDIR/repl/ && \
+cd $BASEDIR/repl && \
 npm install && \
 echo "#!/usr/bin/env node" > apiaxle && cat ./apiaxle.js >> apiaxle && \
-chmod a+x apiaxle && ln -s /app/apiaxle/repl/apiaxle /usr/bin/apiaxle && \
+chmod a+x apiaxle && ln -s $BASEDIR/repl/apiaxle /usr/bin/apiaxle && \
 
-mv node_modules /app/
+mv node_modules $BASEDIR/
